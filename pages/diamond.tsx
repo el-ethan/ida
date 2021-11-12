885c443cd9da33676b06f18034145bbabf51d82e
Leila Skinner <beileiwang@gmail.com>
2021-11-12 09:55:20 -0600

    display brown grid

--- pages/diamond.tsx
+++ pages/diamond.tsx
@@ -1,6 +1,38 @@
+
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import { CenteredMain } from '../components/Centered';
+import { checkAnswer } from '../services/checkAnswer';
+
+const BrownSquare = styled.div`
+  height: 100px;
+  width: 100px;
+  background: #5a2f2f;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 400px;
+`;
+
+
 const D = () => {
+  const router = useRouter();
+  const size = 16;
+  const cells = Array.from ({length: size}, (x, i) => i + 1);
+  const diamondIndex = Math.floor(Math.random() * size);
+  console.log(diamondIndex);
+
   return (
-    <h1>Diamond</h1>
+    <CenteredMain>
+      <h1>Dig The Diamond</h1>
+      <Container>
+      {cells.map((c) => {
+        return <BrownSquare onClick={() => checkAnswer("D", c === diamondIndex, router)}>{c}</BrownSquare>
+      })}
+      </Container>
+    </CenteredMain>
   )
 }
 
