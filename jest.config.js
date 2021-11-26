ae32ca4b38976cc1b231d3497134f05b5b3ff3e8
Ethan Skinner <10249884+el-ethan@users.noreply.github.com>
2021-11-26 14:09:26 -0600

    Remove some comments from jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,8 @@
-// jest.config.js
 const nextJest = require('next/jest');
-
-// Providing the path to your Next.js app which will enable loading next.config.js and .env files
 const createJestConfig = nextJest();
 
-// Any custom config you want to pass to Jest
 const customJestConfig = {
+    // Any custom config you want to pass to Jest
 };
-// createJestConfig is exported in this way to ensure that next/jest can load the Next.js config which is async
+
 module.exports = createJestConfig(customJestConfig);
