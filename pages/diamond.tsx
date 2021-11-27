eea4c8defbe060573e5cf00b649b868df33e9047
Leila Skinner <beileiwang@gmail.com>
2021-11-27 16:44:28 -0600

    add relaod page flag to checkAnswer

--- pages/diamond.tsx
+++ pages/diamond.tsx
@@ -20,14 +20,23 @@ const D = () => {
   const size = 16;
   const cells = Array.from ({length: size}, (x, i) => i + 1);
   const diamondCellIndex = Math.ceil(Math.random() * size);
-  console.log(diamondCellIndex);
+  const getGhostCellIndex = () => {
+    let ghostIndex = Math.ceil(Math.random() * size);
+    if (ghostIndex === diamondCellIndex) {
+      return ghostIndex === 1 ? ghostIndex += 1 : ghostIndex -= 1;
+    }
+    return ghostIndex;
+  }
+
+  const ghostCellIndex = getGhostCellIndex();
+  console.log({diamondCellIndex, ghostCellIndex});
 
   return (
     <CenteredMain>
       <h1>Dig The 💎</h1>
       <Container>
       {cells.map((c) => {
-        return <Cell key={c} cellIndex={c} diamondCellIndex={diamondCellIndex}></Cell>
+        return <Cell key={c} cellIndex={c} diamondCellIndex={diamondCellIndex} ghostCellIndex={ghostCellIndex}></Cell>
       })}
       </Container>
     </CenteredMain>
