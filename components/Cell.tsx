24b9f7e0df780e53c4dd3403c8ebc9b7a62d46cd
Leila Skinner <beileiwang@gmail.com>
2021-11-27 16:57:12 -0600

    lint

--- components/Cell.tsx
+++ components/Cell.tsx
@@ -1,69 +1,67 @@
 import { useRouter } from 'next/router';
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { checkAnswer } from '../services/checkAnswer';
 
 interface IBrownSquare {
-  opacity: number;
+    opacity: number;
 }
 
-const BrownSquare = styled.div<IBrownSquare>`
+const BrownSquare =
+    styled.div <
+    IBrownSquare >
+    `
   height: 100px;
   width: 100px;
   background: #640f0f;
   opacity: ${props => props.opacity};
   text-align: center;
 `;
-const Container = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  width: 500px;
-`;
 
 const Diamond = styled.p`
-  font-size: 2rem;
-  opacity: 1;
-`
+    font-size: 2rem;
+    opacity: 1;
+`;
 type CellProps = {
-  cellIndex: number; 
-  diamondCellIndex: number; 
-  ghostCellIndex: number
-}
+    cellIndex: number;
+    diamondCellIndex: number;
+    ghostCellIndex: number;
+};
 
-const Cell = ({cellIndex, diamondCellIndex, ghostCellIndex}: CellProps) => {
-  const router = useRouter();
-  const [digIndex, setDigIndex] = useState(0);
-  const [squareOpacity, setSquareOpacity] = useState(0.8);
-  useEffect(() => {
-    if(getDiamond()) {
-      checkAnswer("D", true, router);
-    }
-    if(getGhost()) {
-      checkAnswer("D", false, router, true);
-    }
-  });
+const Cell = ({ cellIndex, diamondCellIndex, ghostCellIndex }: CellProps) => {
+    const router = useRouter();
+    const [digIndex, setDigIndex] = useState(0);
+    const [squareOpacity, setSquareOpacity] = useState(0.8);
+    useEffect(() => {
+        if (getDiamond()) {
+            checkAnswer('D', true, router);
+        }
+        if (getGhost()) {
+            checkAnswer('D', false, router, true);
+        }
+    });
 
-  const handleClick = () => {
-    if(digIndex < 5) {
-      setDigIndex((preIndex) => preIndex + 1)
-      setSquareOpacity((preOpacity) => preOpacity + 0.04)
-    }
-    console.log({cellIndex, digIndex});
-  }
+    const handleClick = () => {
+        if (digIndex < 5) {
+            setDigIndex(preIndex => preIndex + 1);
+            setSquareOpacity(preOpacity => preOpacity + 0.04);
+        }
+        console.log({ cellIndex, digIndex });
+    };
 
-  const getDiamond = () => {
-    return cellIndex === diamondCellIndex && digIndex === 5;
-  }
-  const getGhost = () => {
-    return cellIndex === ghostCellIndex && digIndex === 5;
-  }
+    const getDiamond = () => {
+        return cellIndex === diamondCellIndex && digIndex === 5;
+    };
+    const getGhost = () => {
+        return cellIndex === ghostCellIndex && digIndex === 5;
+    };
 
-  return (
-    <BrownSquare opacity={squareOpacity} onClick={handleClick}>
-    {getDiamond() && <Diamond>{`💎`}</Diamond>}
-    {getGhost() && <Diamond>{`👻`}</Diamond>}
-    </BrownSquare>
-  )
-}
+    return (
+        <BrownSquare opacity={squareOpacity} onClick={handleClick}>
+            {getDiamond() && <Diamond>{`💎`}</Diamond>}
+            {getGhost() && <Diamond>{`👻`}</Diamond>}
+        </BrownSquare>
+    );
+};
 
 export default Cell;
--- pages/diamond.tsx
+++ pages/diamond.tsx
@@ -3,44 +3,50 @@ import Cell from '../components/Cell';
 import { CenteredMain } from '../components/Centered';
 
 const BrownSquare = styled.div`
-  height: 100px;
-  width: 100px;
-  background: #7e4e4e;
-  border: 1px solid black;
+    height: 100px;
+    width: 100px;
+    background: #7e4e4e;
+    border: 1px solid black;
 `;
 
 const Container = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  width: 400px;
+    display: flex;
+    flex-wrap: wrap;
+    width: 400px;
 `;
 
-
 const D = () => {
-  const size = 16;
-  const cells = Array.from ({length: size}, (x, i) => i + 1);
-  const diamondCellIndex = Math.ceil(Math.random() * size);
-  const getGhostCellIndex = () => {
-    let ghostIndex = Math.ceil(Math.random() * size);
-    if (ghostIndex === diamondCellIndex) {
-      return ghostIndex === 1 ? ghostIndex += 1 : ghostIndex -= 1;
-    }
-    return ghostIndex;
-  }
+    const size = 16;
+    const cells = Array.from({ length: size }, (x, i) => i + 1);
+    const diamondCellIndex = Math.ceil(Math.random() * size);
+    const getGhostCellIndex = () => {
+        let ghostIndex = Math.ceil(Math.random() * size);
+        if (ghostIndex === diamondCellIndex) {
+            return ghostIndex === 1 ? (ghostIndex += 1) : (ghostIndex -= 1);
+        }
+        return ghostIndex;
+    };
 
-  const ghostCellIndex = getGhostCellIndex();
-  console.log({diamondCellIndex, ghostCellIndex});
+    const ghostCellIndex = getGhostCellIndex();
+    // console.log({diamondCellIndex, ghostCellIndex});
 
-  return (
-    <CenteredMain>
-      <h1>Dig The 💎</h1>
-      <Container>
-      {cells.map((c) => {
-        return <Cell key={c} cellIndex={c} diamondCellIndex={diamondCellIndex} ghostCellIndex={ghostCellIndex}></Cell>
-      })}
-      </Container>
-    </CenteredMain>
-  )
-}
+    return (
+        <CenteredMain>
+            <h1>Dig The 💎</h1>
+            <Container>
+                {cells.map(c => {
+                    return (
+                        <Cell
+                            key={c}
+                            cellIndex={c}
+                            diamondCellIndex={diamondCellIndex}
+                            ghostCellIndex={ghostCellIndex}
+                        />
+                    );
+                })}
+            </Container>
+        </CenteredMain>
+    );
+};
 
 export default D;
